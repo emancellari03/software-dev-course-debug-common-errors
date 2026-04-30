6813d6bf578905f30519bf663fe444539534bfaa
Egi Mancellari <mancellariegi@gmail.com>
2026-04-30 02:46:59 -0500

    fixed syntax, runtime, and logic errors

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,7 +24,8 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+// Syntax Error: missing closing quote and parenthesis
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
 
@@ -33,7 +34,8 @@ console.log("Welcome to the bootcamp
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+// Runtime Error: string should be a number
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
@@ -42,7 +44,7 @@ for (let i = 0; i < numbers.length; i++) {
 // What’s Wrong?
 
 
-
+// Logic Error: true and false were reversed
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
@@ -51,10 +53,10 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
